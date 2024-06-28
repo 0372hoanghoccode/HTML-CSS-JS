62d9956afe6e101790add2a655dcb2131246b208
0372hoanghoccode <146009735+0372hoanghoccode@users.noreply.github.com>
2024-06-29 03:34:43 +0700

    push

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,14 +1,20 @@
-const panels = document.querySelectorAll('.panel')
+// Lấy tất cả các phần tử có class là 'panel' và lưu vào mảng panels
+const panels = document.querySelectorAll('.panel');
 
+// Duyệt qua từng phần tử panel và thêm sự kiện 'click'
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+        // Khi click vào một panel, gọi hàm removeActiveClasses để xóa class 'active' khỏi tất cả các panel
+        removeActiveClasses();
+        // Sau đó, thêm class 'active' vào panel được click
+        panel.classList.add('active');
+    });
+});
 
+// Hàm để xóa class 'active' khỏi tất cả các panel
 function removeActiveClasses() {
     panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+        panel.classList.remove('active');
+    });
 }
+
