8c8720f62908b769f293837d89fc04154b11cf42
0372hoanghoccode <146009735+0372hoanghoccode@users.noreply.github.com>
2024-07-12 23:42:19 +0700

    push

--- form-input-wave/script.js
+++ form-input-wave/script.js
@@ -1,8 +1,14 @@
 const labels = document.querySelectorAll('.form-control label')
+/* Lấy tất cả các nhãn (label) bên trong phần tử có lớp (class) là 'form-control' và lưu chúng vào biến labels. */
 
 labels.forEach(label => {
     label.innerHTML = label.innerText
+        /* Với mỗi nhãn (label), lấy nội dung văn bản của nó (innerText). */
         .split('')
+        /* Tách chuỗi văn bản thành một mảng các ký tự. */
         .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        /* Với mỗi ký tự trong mảng, tạo một phần tử span và đặt thuộc tính delay cho chuyển đổi (transition-delay) dựa trên chỉ số của ký tự. */
         .join('')
+        /* Ghép lại các phần tử span thành một chuỗi HTML. */
 })
+/* Áp dụng sự thay đổi này cho mỗi nhãn (label) trong danh sách labels. */
