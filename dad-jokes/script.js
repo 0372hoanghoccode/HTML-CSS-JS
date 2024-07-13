65dafcecc8182184ee6761e740dcc3e04aef0103
0372hoanghoccode <146009735+0372hoanghoccode@users.noreply.github.com>
2024-07-13 23:50:24 +0700

    push lần nữa

--- dad-jokes/script.js
+++ dad-jokes/script.js
@@ -1,36 +1,36 @@
-const jokeEl = document.getElementById('joke')
-const jokeBtn = document.getElementById('jokeBtn')
+const jokeEl = document.getElementById('joke')  // Tìm và lưu phần tử có id là 'joke' vào biến jokeEl
+const jokeBtn = document.getElementById('jokeBtn')  // Tìm và lưu phần tử có id là 'jokeBtn' vào biến jokeBtn
 
-jokeBtn.addEventListener('click', generateJoke)
+jokeBtn.addEventListener('click', generateJoke)  // Thêm sự kiện 'click' cho jokeBtn để gọi hàm generateJoke khi nhấn vào nút
 
-generateJoke()
+generateJoke()  // Gọi hàm generateJoke ngay khi trang được tải để hiển thị một câu đùa ban đầu
 
-// USING ASYNC/AWAIT
+// SỬ DỤNG ASYNC/AWAIT
 async function generateJoke() {
   const config = {
     headers: {
-      Accept: 'application/json',
+      Accept: 'application/json',  // Đặt header để yêu cầu định dạng trả về là JSON
     },
   }
 
-  const res = await fetch('https://icanhazdadjoke.com', config)
+  const res = await fetch('https://icanhazdadjoke.com', config)  // Gửi yêu cầu fetch đến API với cấu hình header, đợi cho đến khi nhận được phản hồi
+  const data = await res.json()  // Chuyển đổi phản hồi thành định dạng JSON, đợi cho đến khi quá trình hoàn thành
 
-  const data = await res.json()
-
-  jokeEl.innerHTML = data.joke
+  jokeEl.innerHTML = data.joke  // Cập nhật nội dung của jokeEl với câu đùa nhận được từ API
 }
 
-// USING .then()
-// function generateJoke() {
-//   const config = {
-//     headers: {
-//       Accept: 'application/json',
-//     },
-//   }
+// thích em theo nhiều kiểu nên code này anh viết theo nhiều kiểu nhưng tóm lại là một
+// SỬ DỤNG .then()
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',  // Đặt header để yêu cầu định dạng trả về là JSON
+    },
+  }
 
-//   fetch('https://icanhazdadjoke.com', config)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       jokeEl.innerHTML = data.joke
-//     })
-// }
+  fetch('https://icanhazdadjoke.com', config)  // Gửi yêu cầu fetch đến API với cấu hình header
+    .then((res) => res.json())  // Khi nhận được phản hồi, chuyển đổi phản hồi thành JSON
+    .then((data) => {  // Khi việc chuyển đổi JSON hoàn thành
+      jokeEl.innerHTML = data.joke  // Cập nhật nội dung của jokeEl với câu đùa nhận được từ API
+    })
+}
