6d97a57ef80ebd090cb53f1dcd496bb1c2e43e75
0372hoanghoccode <146009735+0372hoanghoccode@users.noreply.github.com>
2024-07-16 22:48:49 +0700

    push my version

--- faq-collapse/script.js
+++ faq-collapse/script.js
@@ -1,7 +1,10 @@
 const toggles = document.querySelectorAll('.faq-toggle')
 
+// Lặp qua tất cả các phần tử có lớp 'toggle'
 toggles.forEach(toggle => {
+    // Thêm sự kiện 'click' cho mỗi phần tử 'toggle'
     toggle.addEventListener('click', () => {
-        toggle.parentNode.classList.toggle('active')
-    })
-})
+        // Khi click vào, thay đổi lớp 'active' của phần tử cha
+        toggle.parentNode.classList.toggle('active');
+    });
+});
