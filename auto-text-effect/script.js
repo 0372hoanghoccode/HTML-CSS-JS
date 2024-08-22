6595cb675265e93ba86c048565b83bebd13df061
0372hoanghoccode <146009735+0372hoanghoccode@users.noreply.github.com>
2024-08-22 23:36:26 +0700

    done

--- auto-text-effect/script.js
+++ auto-text-effect/script.js
@@ -1,22 +1,17 @@
-const textEl = document.getElementById('text')
-const speedEl = document.getElementById('speed')
-const text = 'We Love Programming!'
-let idx = 1
-let speed = 300 / speedEl.value
-
-writeText()
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+const text = 'Heil Hitler!';
+let idx = 1;
+let speed = 300 / speedEl.value;
 
 function writeText() {
-    textEl.innerText = text.slice(0, idx)
-
-    idx++
-
-    if(idx > text.length) {
-        idx = 1
-    }
-
-    setTimeout(writeText, speed)
+  textEl.innerText = text.slice(0, idx);
+  idx = (idx % text.length) + 1;
+  setTimeout(writeText, speed);
 }
 
+writeText();
 
-speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
+speedEl.addEventListener('input', () => {
+  speed = 300 / speedEl.value;
+});
